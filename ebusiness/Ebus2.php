3c56ee1f689379b82b99ef0875266e3fdcacbcf6
James Donovan <117378263@umail.ucc.ie>
2018-03-01 19:13:04 +0000

    multiple edits with css

--- ebusiness/Ebus2.php
+++ ebusiness/Ebus2.php
@@ -2,6 +2,66 @@
 <html>
     <head>
         
+        
+         <style>
+         
+          .bottom_img{
+        position: fixed;
+        width: 560px;
+    }
+         
+      body{
+    background:url('http://raslras.com/images/features_bg.jpeg') no-repeat;
+    background-size: cover;
+    font-family: arial;
+    color: white;
+}
+   
+
+    .button_proceed{
+  height:38px;
+  background:#0f847c;
+  border:0;
+  padding-left: 20px;
+  padding-right:20px;
+  color:#ffffff;
+  cursor: pointer;
+}
+    .Validate{
+border: 2px solid #f44336;
+    background-color: white; 
+    color: blue;
+    padding: 10px 12px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 14px;
+    margin: 4px 2px;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+}
+ .hover:hover{
+     background-color: #0000FF;
+    color: white; 
+ }
+
+</style>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         <title> Enter Details</title>
         
         
@@ -9,13 +69,27 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     </head>
     
-    <body>
+    <body align="center">
         
         <h4>Please enter your payment details.</h4>
-        
+
             <br />
             
-            <form method = "POST" action = "Ebus3.php">
+            <form align="center" method = "POST" action = "Ebus3.php">
+                
+                
+                First name:<br>
+<input type="text" name="firstname">
+<br>
+Last name:<br>
+<input type="text" name="lastname">
+<br><br>
+
+                 E-mail:
+  <input type="email" name="email">
+  
+  <br/><br/>
+  
                 
                 <label for="user_pin">
                      PIN 
@@ -23,18 +97,26 @@
                 
                 <input type="password" id="user_pin" placeholder="Card Pin" maxlength="4">
                     
-                <button type="Submit" id="btnPurchase" disabled> 
+                <button class ="button_proceed" type="Submit" id="btnPurchase" disabled> 
                     Proceed with Purchase 
                 </button>
+                
+               
+ 
+
+
                 
             </form>
             
             <br />
             
-            <button onClick="validateDetails()"> Validate </button>
+            <button  class ="Validate hover" onClick="validateDetails()"> Validate </button>
         
         <script type="text/javascript" src="ebus2_validator.js"></script>
         
+        <br/>
+        
+         <img align="center" class="bottom_img" src="http://www.globallinkic.org/images/interface/bodyFooter_img1.png"/>
         
     </body>
     
